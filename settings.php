3fc45089489b50ff13e6c98a25dd32c10da61007
anes89cok <anes89cok@gmail.com>
2015-03-04 10:11:54 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="635 E. Ogden Avenue"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Downers+Grove";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica="IL 60515"; // AKo ne znate skracenicu ostavite praznu
+	$telephone=6309682812;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
